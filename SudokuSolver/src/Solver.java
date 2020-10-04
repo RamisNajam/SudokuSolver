81ba9b40747762349e7570b7068db4adc337291d
RamisNajam <30279853+RamisNajam@users.noreply.github.com>
2020-10-04 18:21:50 -0400

    Update Solver.java

--- SudokuSolver/src/Solver.java
+++ SudokuSolver/src/Solver.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
  * Created by Ramis on 2017-08-17.
  
  This is the only class that needs to be accessed. Transcribe the digits of the Sudoku puzzle row by row
- (going left to right) without any spaces. Blank squares are to be inputted as zeroes. This is to be placed in line 23 for the values attribute.
+ (going left to right) without any spaces. Blank squares are to be inputted as zeroes. This is to be placed in line 25 for the values attribute.
  Next, run the code, and it will output a formatted, solved version of the grid. An example string is
  included there.
  */
